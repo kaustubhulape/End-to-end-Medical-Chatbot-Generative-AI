90d9bf4d250fe3cef5e0409bbe59156c3750860c
Kaustubh Ulape <kaustubh4594@gmail.com>
2025-06-18 00:19:23 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Kaustubh Ulape',
+    author_email = 'kaustubh4594@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
